4adc7bc7cdd6a2a333a02e0f044534245fe0f42c
Kyle Vu <ktvu@hbuhsd.edu>
2025-01-29 19:46:04 +0000

    added difficulty levels

--- src/main.c
+++ src/main.c
@@ -3,27 +3,43 @@
 int main(void)
 {
     srand(time(0));
-    char* result = calloc(SIZE+1, sizeof(char));
+    printf("Choose your difficulty:\n1. Easy\n2. Medium\n3. Hard\n> ");
+    int diff = 0;
+    int size = 4;
+    scanf("%d", &diff);
+    switch (diff) {
+        case 2:
+            size = 6;
+            break;
+        case 3:
+            size = 8;
+            break;
+        case 1:
+        default:
+            size = 4;
+            break;
+    }
+    char* result = calloc(size+1, sizeof(char));
     uint8_t num_guess = 0;
-    int* code = MakeCode(SIZE);
-    int* guess = calloc(SIZE, sizeof(int));
+    int* code = MakeCode(size);
+    int* guess = calloc(size, sizeof(int));
 
     printf("Answer is ");
-    for (int i = 0; i < SIZE; ++i)
+    for (int i = 0; i < size; ++i)
     {
         printf("%d ", code[i]);
     }
 
     while (strcmp(result, "OOOO"))
     {
-        printf("\nEnter your guess\n");
-        for (size_t i = 0; i < SIZE; ++i)
+        printf("\nEnter your guess (%d numbers)\n", size);
+        for (size_t i = 0; i < size; ++i)
         {
             scanf("%d", &guess[i]);
         }
 
         free(result);
-        result = CheckCode(guess, code, SIZE);
+        result = CheckCode(guess, code, size);
         printf("%s\n", result);
         ++num_guess;
     }
