b401c3039270b8b44e01f74c5712304d3d660ff0
Kyle Vu <ktvu@hbuhsd.edu>
2025-02-04 06:48:47 +0000

    made braces consistent

--- src/mastermind.c
+++ src/mastermind.c
@@ -16,26 +16,34 @@ int* MakeCode(size_t size)
 char* CheckCode(int* guess, int* code, size_t const size)
 {
     char* result = (char*) calloc((size+1), sizeof(char));
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; i < size; ++i)
+    {
         result[i] = 'X';
-        if (guess[i] == code[i]) {
+        if (guess[i] == code[i]
+        {
             result[i] = 'O';
-        } else if (Contains(code, guess[i], size)) {
+        }
+        else if (Contains(code, guess[i], size))
+        {
             result[i] = '+';
         }
     }
     return result;
 }
 
-int DifficultySize(int const diff) {
+int DifficultySize(int const diff)
+{
     if (diff == 2) return 6;
     if (diff == 3) return 8;
     return 4;
 }
 
-unsigned int Contains(int const * const code, int target, size_t const size) {
-    for (int i = 0; i < size; ++i) {
-        if (code[i] == target) {
+unsigned int Contains(int const * const code, int target, size_t const size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        if (code[i] == target)
+        {
             return 1;
         }
     }
